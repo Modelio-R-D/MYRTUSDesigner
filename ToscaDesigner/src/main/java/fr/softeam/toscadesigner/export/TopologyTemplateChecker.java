eecb47554c057cf6fe6625267537b560a7e9090b
Amina-Moussaoui <87063598+Amina-Moussaoui@users.noreply.github.com>
2025-01-23 11:09:04 +0100

    Revert "temporary: deleting export module" + adding github dependency
    This reverts commit fa952b9aa6085853cc735fbf9def007d6a883722.

---
+++ ToscaDesigner/src/main/java/fr/softeam/toscadesigner/export/TopologyTemplateChecker.java
@@ -0,0 +1,28 @@
+package fr.softeam.toscadesigner.export;
+
+import org.modelio.api.module.context.log.ILogService;
+import org.modelio.metamodel.uml.infrastructure.ModelElement;
+import org.modelio.vcore.smkernel.mapi.MObject;
+
+import fr.softeam.toscadesigner.api.IToscaDesignerPeerModule;
+import fr.softeam.toscadesigner.api.tosca.standard.package_.TTopologyTemplate;
+
+public class TopologyTemplateChecker implements ToscaObjectTypeChecker {
+
+	@Override
+	public boolean isTypeOf(MObject object) {
+		
+		if(!(object instanceof ModelElement)) {
+			return false;
+		}
+		ModelElement element = (ModelElement) object;
+		return element.isStereotyped(IToscaDesignerPeerModule.MODULE_NAME, TTopologyTemplate.STEREOTYPE_NAME);
+		
+	}
+	
+	@Override
+	public AbstractToscaFileGenerator createGenerator(ILogService logger) {
+		
+		return new ToscaFileGenerator(logger);
+	}
+}
